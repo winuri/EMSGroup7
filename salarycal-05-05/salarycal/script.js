0ea8b9a25636829d7146b24ff54146aa68453522
Winuri Sathma <sathmawinuri@gmail.com>
2024-07-09 17:07:26 +0530

    update script

--- salarycal-05-05/salarycal/script.js
+++ salarycal-05-05/salarycal/script.js
@@ -1,4 +1,3 @@
-
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 
@@ -20,15 +19,3 @@ setInterval(nextSlide, 3000); // Change slide every 3 seconds
 
 // Initial display
 showSlide(currentSlide);
-
-function saveData( emp_id) {
-    // Store data in sessionStorage
-    sessionStorage.setItem('EMP_ID', emp_id);
-
-    // Provide feedback to the user
-    document.getElementById('output').innerText = 'Data saved successfully!';
-    console.log("Data saved successfully!");	
-
-    console.log(sessionStorage.getItem('EMP_ID'));
-}
-
