1d501bc1ba06ee450ebc62549c5eeaf2e371a06c
Winuri Sathma <sathmawinuri@gmail.com>
2024-07-09 17:13:11 +0530

    update leave

--- salarycal-05-05/salarycal/submit_leave.php
+++ salarycal-05-05/salarycal/submit_leave.php
@@ -25,7 +25,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Execute the statement
     if ($stmt->execute()) {
-        echo "success";
         header("Location: leave.html?status=success");
     } else {
         header("Location: leave.html?status=error");
